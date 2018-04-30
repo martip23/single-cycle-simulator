731cf73143cf9e3c85cd7607a5b8b817d0b0efa0
martip23 <patrickmartinez77@gmail.com>
2018-04-29 22:20:28 -0500

    Added translation of registers to index

--- packages/src/sincySimulator/Utilities.java
+++ packages/src/sincySimulator/Utilities.java
@@ -1,5 +1,7 @@
 package sincySimulator;
 
+import java.util.Scanner;
+
 /**
  * A series of public utilities to be used by the Sincy processor
  * @author martip23
@@ -141,6 +143,7 @@ public class Utilities {
 				}
 				break;
 		}
+		s.close();
 		return op+rs+rt+rd+shamt+funct+imm;
 	}
 	
@@ -261,6 +264,7 @@ public class Utilities {
 	 * @return - register index number
 	 */
 	static public int registerCodeToInt(String reg) {
+		reg.toLowerCase();
 		switch(reg) {
 			case "$zero":
 				return 0;
@@ -269,7 +273,63 @@ public class Utilities {
 			case "$v0":
 				return 2;
 			case "$v1":
-				return 3;		
+				return 3;
+			case "$a0":
+				return 4;
+			case "$a1":
+				return 5;
+			case "$a2":
+				return 6;
+			case "$a3":
+				return 7;	
+			case "$t0":
+				return 8;
+			case "$t1":
+				return 9;
+			case "$t2":
+				return 10;
+			case "$t3":
+				return 11;	
+			case "$t4":
+				return 12;
+			case "$t5":
+				return 13;
+			case "$t6":
+				return 14;
+			case "$t7":
+				return 15;	
+			case "$s0":
+				return 16;
+			case "$s1":
+				return 17;
+			case "$s2":
+				return 18;
+			case "$s3":
+				return 19;	
+			case "$s4":
+				return 20;
+			case "$s5":
+				return 21;
+			case "$s6":
+				return 22;
+			case "$s7":
+				return 23;	
+			case "$t8":
+				return 24;
+			case "$t9":
+				return 25;
+			case "$k0":
+				return 26;
+			case "$k1":
+				return 27;
+			case "$gp":
+				return 28;
+			case "$sp":
+				return 29;
+			case "$fp":
+				return 30;
+			case "$ra":
+				return 31;	
 		}
 		return -1;
 	}
